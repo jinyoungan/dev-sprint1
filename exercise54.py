4afcd5cd4783960619592bc678d0915d64dc6454
jinyoungan <jinny117@gmail.com>
2014-06-19 20:31:19 -0400

    lab02

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,13 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Jin
+
+def is_triangle():
+	a = int(raw_input("a lenth?\n"))
+	b = int(raw_input("b lenth?\n"))
+	c = int(raw_input("c lenth?\n"))
+	if a > (b + c) or b > (a + c) or c > (a + b) :
+		print "No"
+	else :
+		print "Yes"
+is_triangle()
+
